ab7e1a4accc5dead3e1dd2b4a825520f707c6fdb
logicfirst <slee@logicfirstllc.com>
2020-12-21 10:40:09 -0500

    Completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Coupon.destroy_all
+
+cp1 = Coupon.create(coupon_code: "10", store: "Walmart")
+cp2 = Coupon.create(coupon_code: "08", store: "Walmart")
+cp3 = Coupon.create(coupon_code: "07", store: "K-Mart")
+cp4 = Coupon.create(coupon_code: "10", store: "Dick's")
